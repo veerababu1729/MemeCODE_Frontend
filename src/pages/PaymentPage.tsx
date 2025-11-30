c7e19264a625cdd12385ed9cb1a38626ac6ae95a
veerababu1729 <20A91A0546@aec.edu.in>
2025-11-30 12:24:20 +0530

    coupon functionalityadded

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -281,7 +281,7 @@ const PaymentPage = () => {
                 <div className="text-center">
                   <div className="bg-gradient-to-r from-yellow-100 via-yellow-50 to-yellow-100 border-2 border-yellow-300 rounded-lg p-3 mb-2 shadow-lg">
                     <p className="text-sm font-bold text-yellow-800 animate-bounce">
-                      🎉 Any Coupon Code? 🎉
+                      🎉 Apply coupon code to get at ₹199 🎉
                     </p>
 
                   </div>
